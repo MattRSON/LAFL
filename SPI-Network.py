6f5c3600aea8062ad0e3bce1fcb852e77d007767
MattRSON <mattrawson02@outlook.com>
2024-01-28 23:14:23 -0700

    Added support for sending data over the network

--- SPI-Network.py
+++ SPI-Network.py
@@ -1,20 +1,21 @@
 # Imports Libraries
 import spidev
 import RPi.GPIO as GPIO
-import time
+import socket
+#import time
 
 # Use the pin numbers on the board
 GPIO.setmode(GPIO.BOARD)
 
 # Chip select pin definitions
-#Select1 = 25
+Select1 = 22
 
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 # Use Spi bus 0
 bus = 0
 
-# Chip select pin (Will probably be ignored for other pins)
+# Chip select pin (Will be ignored for other pins)
 device = 1
 
 # Enable spi
@@ -22,17 +23,28 @@ spi = spidev.SpiDev()
 
 # Open a connections to the device
 spi.open(bus, device)
-GPIO.output(25, GPIO.HIGH)
+GPIO.output(22, GPIO.HIGH) # Enable fake CS pin
+
 # Set the SPI speed and mode
 spi.max_speed_hz = 1000000 #1Mhz
 spi.mode = 0
 
+# Setup tcp server
+HOST = ''
+PORT = 65432
 
-while True:
-    # try to read 16 bits from the spi bus
-    GPIO.output(25, GPIO.LOW) # Test fake chip select pin
-    Data1 = spi.readbytes(2)
-    ADC = Data1[0]*256+Data1[1]
-    GPIO.output(25, GPIO.HIGH)
-    print(ADC)
-    time.sleep(1)
+# Setup socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    # When connection is made start collecting and sending data
+    with conn:
+        while True:
+            # try to read 16 bits from the spi bus and send it over network
+            GPIO.output(22, GPIO.LOW)
+            Data1 = spi.readbytes(2)
+            ADC = Data1[0]*256+Data1[1]
+            GPIO.output(22, GPIO.HIGH)
+            conn.sendall(ADC)
+    
