3e8379a365c667eda6127983b692a12ccb99196f
MattRSON <mattrawson02@outlook.com>
2024-01-29 00:33:17 -0700

    update

--- NetworkTestScripts/MatlabClient.m
+++ NetworkTestScripts/MatlabClient.m
@@ -1,11 +1,11 @@
 %% Test of the network transfer
 
-t = tcpclient("LAFL",65433)
-data = [0x00,0x00];
+t = tcpclient("LAFL",65432)
+%data = [0x00,0x00];
 
 while 1
-    write(t,data)
-    read(t,1,"int16")
+    %write(t,data)
+    read(t,1,"uint16")
 end
 
 %clear t
