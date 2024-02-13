ab11a74cf1ebd60a4c36edc9b7728127ff2ace89
MattRSON <mattrawson02@outlook.com>
2024-02-13 12:42:12 -0700

    The graph is live with a 8 second show

--- LaptopPython.py
+++ LaptopPython.py
@@ -5,16 +5,19 @@ from matplotlib.animation import FuncAnimation
 from collections import deque
 import signal
 import sys
+import numpy as np
 
 HOST = "LAFL"   # Hostname
 PORT = 65433    # Port
 
-MAX_DATA_POINTS = 10000
+MAX_DATA_POINTS = 400
 
 # Initialize plot
 fig, ax = plt.subplots()
 x_data = deque(maxlen=MAX_DATA_POINTS)
+x_data = range(MAX_DATA_POINTS)
 y_data = deque(maxlen=MAX_DATA_POINTS)
+y_data.extend(np.ones(MAX_DATA_POINTS))
 line, = ax.plot(x_data, y_data)
 
 # Global Data Lock
@@ -40,8 +43,8 @@ def nodeA():
 def update_plot(frame):
     with data_lock:
         value = data_value
-    x_data.append(len(x_data) + 1)
-    y_data.append(value)
+    y_data.appendleft(value)
+    y_data.pop
     line.set_xdata(x_data)
     line.set_ydata(y_data)
 
