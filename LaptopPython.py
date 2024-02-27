1234bff803241173b0d9dacb04149382afd3f6aa
MattRSON <mattrawson02@outlook.com>
2024-02-27 10:40:03 -0700

    Graph works and all code commented

--- LaptopPython.py
+++ LaptopPython.py
@@ -1,66 +1,68 @@
 # Written my Jai Bhullar and Mathew Rawson
 
-import socket
-import threading
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from collections import deque
-import signal
-import sys
-import numpy as np
+import socket # For network connection
+import threading # For threading used for plot
+import matplotlib.pyplot as plt # For the plot
+from matplotlib.animation import FuncAnimation # For continuous updates to the plot
+from collections import deque # Data structure. Allows us to add and remove from both sides of the array
+import signal # Used to safe shutdown the script
+import sys # Also used to safe shutdown the script
+import numpy as np # For extra number manipulation
 
+## Setting up the network with the name of computer and what port its sending data on
 HOST = "LAFL"   # Hostname
-PORT = 65433    # Port
+PORT = 65432    # Port
 
-MAX_DATA_POINTS = 400
+MAX_DATA_POINTS = 400 # 4 seconds of points
 
-# Initialize plot
-fig, ax = plt.subplots()
-x_data = deque(maxlen=MAX_DATA_POINTS)
-x_data = range(MAX_DATA_POINTS)
-y_data = deque(maxlen=MAX_DATA_POINTS)
-y_data.extend(np.ones(MAX_DATA_POINTS))
-line, = ax.plot(x_data, y_data)
+## Initialize plot
+fig, ax = plt.subplots() # Starts the plot
+x_data = deque(maxlen=MAX_DATA_POINTS) # Sets up the array givin the max points its allowed to have
+x_data = range(MAX_DATA_POINTS) # Fills the array with numbers 0:Max points for the x axis
+y_data = deque(maxlen=MAX_DATA_POINTS) # Sets up the array the same way as x_data
+y_data.extend(np.ones(MAX_DATA_POINTS)) # Fills the whole thing with ones to start
+line, = ax.plot(x_data, y_data) # Graph the data points as a line
 
 # Global Data Lock
-data_lock = threading.Lock()
-data_value = 0
+data_lock = threading.Lock() # Prevents both threads from trying to modify a variable at the same time
+data_value = 0 # Initializes the global data variable. This is the data from the ADCs
 
 # Thread to recieve data from PI (No Delay)
 def nodeA():
-    global data_value
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        while True:
-            data = s.recv(2)
-            if not data:
+    global data_value # Grabs the global variable
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # Checks to see if the Rpi server is running
+        s.connect((HOST, PORT)) # Tries to connect to the sever
+        while True: # If it can
+            data = s.recv(2) # Grab the data the server is sending
+            if not data: # If the server stops sending data then close the connection
                 break
-            value = int.from_bytes(data, byteorder='big')
-            with data_lock:
-                data_value = value
+            value = int.from_bytes(data, byteorder='big') # Turn the binary stream into an int
+            with data_lock: # If this thread has control of the variable 
+                data_value = value # Update it
 
 
 
 # Function to update plot in animation
 def update_plot(frame):
-    with data_lock:
-        value = data_value
-    y_data.appendleft(value)
-    y_data.pop
-    line.set_xdata(x_data)
+    with data_lock: # If the thread has control of the variable
+        value = data_value # Grab the mose recent update
+    y_data.appendleft(value) # Add it to the left of the array ei most recent data
+    y_data.pop # Remove the right value in the array ei oldest data
+    line.set_xdata(x_data) # Graph the x and y values as a line
     line.set_ydata(y_data)
 
-    ax.set_ylim(0,70000)
+    ax.set_ylim(0,4100) # Sets the limits of the graph
 
-    ax.relim()
-    ax.autoscale_view()
-
+# Function to shutdown script safely
 def signal_handler(sig, frame):
     print("ABORTING")
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# Sets how often the graph updates (The graph doesn't grab every value sent by the pi)
+# Its to slow to do that
+# But all of the data is still recived in real time and so we can process it quickly
 ani = FuncAnimation(fig, update_plot, interval=1)
 
 # Thread creation and start
@@ -68,4 +70,5 @@ RECV_NODE = threading.Thread(target=nodeA)
 RECV_NODE.daemon = True
 RECV_NODE.start()
 
+# Show the plot
 plt.show()
--- README.md
+++ README.md
@@ -6,9 +6,12 @@ The code base for both the Python script running on the RPI and on a laptop
 
 Things to do
 - Fix graph big/little bit mixup
+    -- Fixed 2/27/24 (Both need to be set to big/MSB first)
 - expand graph to all 12 signals
+    - Code on both the pi and laptop
 - Implement FFT code
     - A step for noise reduction
     - common mode rejection??
 - Filtering code
 - Diffrence of phase code
+- UI design
