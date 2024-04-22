b416bcce35caf52ac871da4aec65e2791c81c062
MattRSON <mattrawson02@outlook.com>
2024-04-21 20:40:14 -0600

    Update SPI-NoNetwork.c

--- SPI-NoNetwork.c
+++ SPI-NoNetwork.c
@@ -22,7 +22,8 @@ int main(){
     // Init gpio
     if (gpioInitialise()<0) return -1;
     int handle = 0;
-    uint16_t Data[12];
+    uint16_t Data[1];
+    uint16_t BulkData[12];
     handle = spiOpen(0, 12000000,0);
     // Set pins as outputs
     gpioSetMode(Select1, PI_OUTPUT);
@@ -57,41 +58,53 @@ int main(){
 
 
     gpioWrite(Select1, 0);
-    spiRead(handle, (char*)Data[0],2);
+    spiRead(handle, (char*)Data,2);
     gpioWrite(Select1, 1);
+    BulkData[0] = Data
     gpioWrite(Select2, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select2, 1);
+    BulkData[1] = Data
     gpioWrite(Select3, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select3, 1);
+    BulkData[2] = Data
     gpioWrite(Select4, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select4, 1);
+    BulkData[3] = Data
     gpioWrite(Select5, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select5, 1);
+    BulkData[4] = Data
     gpioWrite(Select6, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select6, 1);
+    BulkData[5] = Data
     gpioWrite(Select7, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select7, 1);
+    BulkData[6] = Data
     gpioWrite(Select8, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select8, 1);
+    BulkData[7] = Data
     gpioWrite(Select9, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select9, 1);
+    BulkData[8] = Data
     gpioWrite(Select10, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select10, 1);
+    BulkData[9] = Data
     gpioWrite(Select11, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select11, 1);
+    BulkData[10] = Data
     gpioWrite(Select12, 0);
     spiRead(handle, (char*)Data,2);
     gpioWrite(Select12, 1);
+    BulkData[11] = Data
     
     for (int i = 0; i < 12; i++) {
         printf("%d ", ADCS[i]);
